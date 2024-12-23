23ed63c82e84156629bbefd0ca24a7ce2016a3cb
Bugra020 <bugrakaya020@gmail.com>
2024-12-23 22:32:41 +0300

    completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,19 +16,19 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local cap = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
+				capabilities = { cap },
 			})
 
 			lspconfig.clangd.setup({
-				capabilities = capabilities,
+				capabilities = cap,
 			})
 
 			lspconfig.pyright.setup({
-				capabilities = capabilities,
+				capabilities = cap,
 			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return   {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"c", "lua", "java", "python", "html", "javascript", "vim", "vimdoc"},
+      --ensure_installed = {"c", "lua", "java", "python", "html", "javascript", "vim", "vimdoc"},
       auto_install = true,
       highlight = {enable = true},
       indent = {enable = true},
